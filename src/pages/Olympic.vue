5604d057a872af9fd9d013bf6d7acd3aa931f4d2
unfound <448217185@qq.com>
2022-02-17 23:05:59 +0800

    feat: 调整摄像头以及加入控制组件

--- src/pages/Olympic.vue
+++ src/pages/Olympic.vue
@@ -14,6 +14,7 @@ import {
     AmbientLight,
     LoadingManager
 } from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import WebGL from 'three/examples/jsm/capabilities/WebGL'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import skyTexture from '../images/sky.jpg'
@@ -37,24 +38,24 @@ onMounted(() => {
         wrapper.value.append(renderer.domElement)
         // 设置摄像机
         const camera = new PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000)
-        camera.position.set(0, 30, 100)
+        camera.position.set(0, 5, 10)
         camera.lookAt(0, 0, 0)
         // 设置场景
         const scene = new Scene()
         scene.background = new TextureLoader().load(skyTexture)
-        scene.fog = new Fog(0xffffff, 10, 100) // 雾气
+        scene.fog = new Fog(0xffffff, 20, 1000) // 雾气
         // 设置阳光
-        const cubeGeometry = new BoxGeometry(0.001, 0.001, 0.001)
-        const cubeMaterial = new MeshLambertMaterial({ color: 0xdc161a})
-        const cube = new Mesh(cubeGeometry, cubeMaterial)
-        cube.position.set(0, 0, 0)
-        // const target = new Object3D()
-        // target.position.set(0, 0, 0)
-        // target.visible = false
+        // const cubeGeometry = new BoxGeometry(0.001, 0.001, 0.001)
+        // const cubeMaterial = new MeshLambertMaterial({ color: 0xdc161a})
+        // const cube = new Mesh(cubeGeometry, cubeMaterial)
+        // cube.position.set(0, 0, 0)
+        const target = new Object3D()
+        target.position.set(0, 0, 0)
+        target.visible = false
         const light = new DirectionalLight(0xffffff, 1)
         light.position.set(16, 16, 8)
         light.castShadow = true // 开启阴影
-        light.target = cube
+        light.target = target
         light.shadow.mapSize.width = 512 * 12
         light.shadow.mapSize.height = 512 * 12
         light.shadow.camera.top = 40
@@ -73,34 +74,35 @@ onMounted(() => {
         const loader = new GLTFLoader(manager)
         const meshes = []
         let land
-        // loader.load(landModel, function (mesh) {
-        //     mesh.scene.traverse(function (child) {
-        //         if (child.isMesh) {
-        //             meshes.push(child)
-        //             child.material.metalness = .1
-        //             child.material.roughness = .8
-        //             // 地面
-        //             if (child.name === 'Mesh_2') {
-        //                 child.material.metalness = .5;
-        //                 child.receiveShadow = true;
-        //             }
-        //             // 围巾
-        //             if (child.name === 'Mesh_17') {
-        //                 child.material.metalness = .2;
-        //                 child.material.roughness = .8;
-        //             }
-        //             // 帽子
-        //             if (child.name === 'Mesh_17') { }
-        //         }
-        //     })
-        //     mesh.scene.rotation.y = Math.PI / 4
-        //     mesh.scene.position.set(15, -20, 0)
-        //     mesh.scene.scale.set(.9, .9, .9)
-        //     land = mesh.scene
-        //     scene.add(land)
-        // }, undefined, (err) => {
-        //     console.error(err)
-        // })
+        loader.load(landModel, function (mesh) {
+            mesh.scene.traverse(function (child) {
+                if (child.isMesh) {
+                    meshes.push(child)
+                    child.material.metalness = .1
+                    child.material.roughness = .8
+                    // 地面
+                    if (child.name === 'Mesh_2') {
+                        child.material.metalness = .5;
+                        child.receiveShadow = true;
+                    }
+                    // 围巾
+                    if (child.name === 'Mesh_17') {
+                        child.material.metalness = .2;
+                        child.material.roughness = .8;
+                    }
+                    // 帽子
+                    if (child.name === 'Mesh_17') { }
+                }
+            })
+            mesh.scene.receiveShadow = true
+            mesh.scene.rotation.y = Math.PI / 4
+            mesh.scene.position.set(15, -20, 0)
+            mesh.scene.scale.set(.9, .9, .9)
+            land = mesh.scene
+            scene.add(land)
+        }, undefined, (err) => {
+            console.error(err)
+        })
         // 冰墩墩
         loader.load(icePandaModel, function (mesh) {
             mesh.scene.traverse(function (child) {
@@ -141,6 +143,19 @@ onMounted(() => {
         }, undefined, (err) => {
             console.error(err)
         })
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.target.set(0, 0, 0);
+        controls.enableDamping = true;
+        controls.enablePan = false;
+        controls.enableZoom = true;
+
+        // 垂直旋转角度限制
+        controls.minPolarAngle = 1.4;
+        controls.maxPolarAngle = 1.8;
+
+        // 水平旋转角度限制
+        controls.minAzimuthAngle = -.8;
+        controls.maxAzimuthAngle = .8;
         function animate () {
             requestAnimationFrame(animate)
             renderer.render(scene, camera)
